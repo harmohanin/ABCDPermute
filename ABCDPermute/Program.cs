3082762a04e16f2475e47524ef106ad10fe5d765
harmo <harmo@HARMOHAN_I7>
2025-08-12 15:31:13 +0530

    Add project files.

---
+++ ABCDPermute/Program.cs
@@ -0,0 +1,30 @@
+﻿class ABCDPermute
+{
+    private string input;
+    ABCDPermute(string input)
+    {
+        this.input = input;
+    }
+
+    void displayPermute()
+    {
+        for (int i = 0; i < input.Length; i++)
+        {
+            string res = string.Empty;
+            res += input[i].ToString();
+            Console.WriteLine(res);
+            for (int j = i + 1; j < input.Length; j++)
+            {
+                res += input[j].ToString();                                
+                Console.WriteLine(res);
+            }
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        string input = "abcd";
+        ABCDPermute permute = new ABCDPermute(input);
+        permute.displayPermute();
+    }
+}
